ecad4ae476d1c38eed553f3c274033f367eb534d
lesiak <lesiak@outlook.com>
2023-02-03 14:13:46 +0100

    [YoutubeDL] Renamed subs scripts

---
+++ YoutubeDL/subs_pl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COLOR_OFF='\033[0m'       # Text Reset
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+
+YouTubeUrl=$1
+if [ -z $YouTubeUrl ]; then
+  echo "Usage: ytdl.sh url"
+  exit
+fi
+
+yt-dlp --sub-langs "pl.*" --write-sub --skip-download $YouTubeUrl
